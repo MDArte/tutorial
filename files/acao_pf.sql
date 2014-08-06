a7ee5115d318fad45088ebcaae9f11ccda68b89c
Felipe Milepe <fmilepe@gmail.com>
2014-08-06 16:44:54 -0300

    Removendo arquivos desnecessários.

--- files/acao_pf.sql
+++
@@ -1,9 +0,0 @@
-insert into acao_pf 
-	select id , 
-	(Select id from perfil where 
-	perfil.sistema_fk=
-		(select id from 
-		sistema where 
-		sistema.nome='sistemaacademico'
-		)
-	) from acao a
